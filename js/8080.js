9c6c7a687aa2e99a7e94be1e88af8505311e2ecd
Siddharth Chopra <sid6327@gmail.com>
2017-01-11 02:39:02 +0530

    Added flag functions and more opcodes.

--- js/8080.js
+++ js/8080.js
@@ -81,7 +81,7 @@ function setOpcodes () {
 		// LXI B, D16
 		CPU.registers[0] = CPU.RAM[CPU.programCounter++];
 		CPU.registers[1] = CPU.RAM[CPU.programCounter++];
-		CPU.registerPairs[0] = (CPU.registers[0] << 8) | CPU.registers[1];
+		syncPairWithRegister(0, 0);
 	};
 	CPU.Instructions[0x02] = function () {
 		// STAX B
@@ -91,48 +91,128 @@ function setOpcodes () {
 	CPU.Instructions[0x03] = function () {
 		// INX B
 		CPU.registerPairs[0]++;
-		CPU.registers[0] = CPU.registerPairs[0] >> 8;
-		CPU.registers[1] = CPU.registerPairs[0] & 0x0f;
-		wait();
+		syncRegisterWithPair(0, 0);
 	};
 	CPU.Instructions[0x04] = function () {
 		// INR B
 		CPU.registers[0]++;
-		CPU.registerPairs[0] = (CPU.registers[0] << 8) | CPU.registers[1];
+		syncPairWithRegister(0, 1, 0);
 		setFlagZ(CPU.registers[0]);
 		setFlagS(CPU.registers[0]);
 		setFlagP(CPU.registers[0]);
-		setFlagAC(CPU.registers[0]);
+		setFlagACplus(CPU.registers[0] - 1, 1);
 	};
 	CPU.Instructions[0x05] = function () {
 		// DCR B
 		CPU.registers[0]--;
-		CPU.registerPairs[0] = (CPU.registers[0] << 8) | CPU.registers[1];
+		syncPairWithRegister(0, 0);
 		setFlagZ(CPU.registers[0]);
 		setFlagS(CPU.registers[0]);
 		setFlagP(CPU.registers[0]);
-		setFlagAC(CPU.registers[0]);	
+		setFlagACminus(CPU.registers[0] + 1, 1);	
+	};
+	CPU.Instructions[0x06] = function () {
+		// MVI B, D8
+		CPU.registers[0] = CPU.RAM[programCounter++];
+		syncPairWithRegister(0, 0);
+	};
+	CPU.Instructions[0x07] = function () {
+		// RLC
+		var d7 = (1 << 7) & CPU.registers[7];
+		d7 = d7 >> 7;
+		CPU.flagC = 1 & CPU.registers[7]; 
+		CPU.registers[7] = CPU.registers[7] << 1;
+	};
+	CPU.Instructions[0x09] = function () {
+		// DAD B
+		var temp = CPU.registerPairs[2];
+		CPU.registerPairs[2] = CPU.registerPairs[0] + CPU.registerPairs[2];
+		syncRegisterWithPair(4, 2);
+		setFlagC(temp + CPU.registerPairs[0], CPU.registerPairs[2]); 
 	};
 }
 
-function setFlagZ (result) {
+function syncPairWithRegister (r, p) {
+	CPU.registerPairs[p] = (CPU.registers[r] << 8) | CPU.registers[r + 1];
+}
 
+function syncRegisterWithPair (r, p) {
+	CPU.registers[r] = CPU.registerPairs[p] >> 8;
+	CPU.registers[r + 1] = (CPU.registerPairs[p] & 0x00ff);
+}
+
+function setFlagZ (result) {
+	if (result == 0) {
+		return CPU.flagZ = 1;
+	}
+	CPU.flagZ = 0;
 }
 
 function setFlagS (result) {
-
+	if (0x80 & result) {
+		return CPU.flagS = 1;
+	}
+	CPU.flagS = 1;
 }
 
 function setFlagP (result) {
-
+	var ones = 0, mask = 0x80;
+	while (mask) {
+		if (mask & result) {
+			ones++;
+		}
+		mask = mask >> 1;
+	}
+	if (ones & 1) {
+		return CPU.flagP = 0;
+	}
+	CPU.flagP = 1;
 }
 
-function setFlagC (result) {
-
+function setFlagC (supposed, actual) {
+	if (supposed != actual) {
+		return CPU.flagC = 1;
+	}
+	CPU.flagC = 0;
 }
 
-function setFlagAC (result) {
+function setFlagACplus (before, value) {
+	var mask = 1;
+	var carry = 0;
+	for (var i = 0; i < 4; i++) {
+		if ((mask & before) + (mask & value) + carry > 1) {
+			carry = 1;
+		} else {
+			carry = 0;
+		}
+		mask = mask << 1;
+	}
+	if (carry) {
+		return CPU.flagAC = 1;
+	}
+	CPU.flagAC = 0;
+}
 
+function setFlagACminus (before, value) {
+	var borrow = 0;
+	var mask = 1 << 3;
+	for (var i = 0; i < 4; i++) {
+		if ((mask & before) < (mask & value)) {
+			if (borrow) {
+				borrow--;
+			} else {
+				borrow = -1;
+				break;
+			}
+		} else if (mask & before) {
+			borrow++;
+		}
+		mask = mask >> 1;
+	}
+	if (borrow == -1) {
+		return CPU.flagAC = 1;
+	}
+	CPU.flagAC = 1;
 }
 
 function wait () {
